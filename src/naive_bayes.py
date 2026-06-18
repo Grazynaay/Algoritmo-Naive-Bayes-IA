f7aacd7e2346f7285e6fab36ad1083c6dfaa7b57
Samuel <andrade.carneiro@mail.uft.edu.br>
2026-06-18 20:53:40 -0300

    melhorei os nomes das variáveis

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -5,85 +5,85 @@ class NaiveBayes:
     """
     Classificador Naive Bayes com atributos numéricos usando distribuição Gaussiana.
     Implementação manual seguindo a fórmula do professor:
-    P[c|x] = P[x|c] * P[c] / P[x]
+    P[classe|x] = P[x|classe] * P[classe] / P[x]
     """
     
     def __init__(self):
         self.classes = None
-        self.class_priors = {}  # P[c]
-        self.feature_stats = {}  # {classe: {'mean': [...], 'std': [...]}}
+        self.priori_classes = {}  # P[classe]
+        self.feature_stats = {}  # {classe: {'media': [...], 'desvio': [...]}}
     
-    def treinar(self, X, y):
+    def treinar(self, dados, rotulos):
         """
         Treinar o modelo Naive Bayes.
         
         Args:
-            X: array 2D (n_samples, n_features) com atributos numéricos
-            y: array 1D (n_samples,) com rótulos de classe
+            dados: array 2D (n_amostras, n_atributos) com atributos numéricos
+            rotulos: array 1D (n_amostras,) com rótulos de classe
         """
-        X = np.array(X)
-        y = np.array(y)
+        dados = np.array(dados)
+        rotulos = np.array(rotulos)
         
-        self.classes = np.unique(y)
-        n_samples, n_features = X.shape
+        self.classes = np.unique(rotulos)
+        n_amostras, n_atributos = dados.shape
         
-        # Para cada classe, calcular P[c] e estatísticas dos atributos
-        for c in self.classes:
-            X_c = X[y == c]  # Amostras da classe c
+        # Para cada classe, calcular P[classe] e estatísticas dos atributos
+        for classe in self.classes:
+            dados_classe = dados[rotulos == classe]  # Amostras da classe
             
-            # P[c] = número de amostras da classe / total
-            self.class_priors[c] = len(X_c) / n_samples
+            # P[classe] = número de amostras da classe / total
+            self.priori_classes[classe] = len(dados_classe) / n_amostras
             
             # Para cada atributo: média e desvio padrão
-            self.feature_stats[c] = {
-                'mean': X_c.mean(axis=0),
-                'std': X_c.std(axis=0)
+            self.feature_stats[classe] = {
+                'media': dados_classe.mean(axis=0),
+                'desvio': dados_classe.std(axis=0)
             }
     
-    def _pdf_gaussiana(self, x, mean, std):
+    def _pdf_gaussiana(self, valor, media, desvio):
         """
-        Calcular PDF gaussiana: P(x|mean, std)
-        Fórmula: (1 / sqrt(2π*std²)) * exp(-(x-mean)² / (2*std²))
+        Calcular PDF gaussiana: P(valor|media, desvio)
+        Fórmula: (1 / sqrt(2π*desvio²)) * exp(-(valor-media)² / (2*desvio²))
         """
-        if std == 0:
+        if desvio == 0:
             return 1.0
         
-        numerador = math.exp(-(x - mean) ** 2 / (2 * std ** 2))
-        denominador = math.sqrt(2 * math.pi * std ** 2)
+        numerador = math.exp(-(valor - media) ** 2 / (2 * desvio ** 2))
+        denominador = math.sqrt(2 * math.pi * desvio ** 2)
         return numerador / denominador
     
-    def predizer(self, X):
+    def predizer(self, dados):
         """
-        Classificar amostras usando MAP (Maximum a Posteriori).
+        Classificar amostras usando MAP (Máximo a Posteriori).
         
         Args:
-            X: array 2D (n_samples, n_features) com amostras a classificar
+            dados: array 2D (n_amostras, n_atributos) com amostras a classificar
             
         Returns:
             array 1D com classes preditas
         """
-        X = np.array(X)
-        predictions = []
+        dados = np.array(dados)
+        predicoes = []
         
-        for x in X:
-            # Para cada classe, calcular log(P[c|x]) = log(P[c]) + log(P[x|c])
-            posteriors = {}
+        for amostra in dados:
+            # Para cada classe, calcular log(P[classe|amostra]) = log(P[classe]) + log(P[amostra|classe])
+            posteriores = {}
             
-            for c in self.classes:
-                # log(P[c])
-                prior = np.log(self.class_priors[c])
+            for classe in self.classes:
+                # log(P[classe])
+                priori = np.log(self.priori_classes[classe])
                 
-                # log(P[x|c]) = Σ log(P[xi|c])
-                likelihood = 0
-                for i, x_i in enumerate(x):
-                    mean = self.feature_stats[c]['mean'][i]
-                    std = self.feature_stats[c]['std'][i]
-                    likelihood += np.log(self._pdf_gaussiana(x_i, mean, std) + 1e-10)  # +eps evita log(0)
+                # log(P[amostra|classe]) = Σ log(P[atributo|classe])
+                verossimilhanca = 0
+                for indice, atributo in enumerate(amostra):
+                    media = self.feature_stats[classe]['media'][indice]
+                    desvio = self.feature_stats[classe]['desvio'][indice]
+                    verossimilhanca += np.log(self._pdf_gaussiana(atributo, media, desvio) + 1e-10)  # +eps evita log(0)
                 
-                # log(P[c|x]) ∝ log(P[c]) + log(P[x|c])
-                posteriors[c] = prior + likelihood
+                # log(P[classe|amostra]) ∝ log(P[classe]) + log(P[amostra|classe])
+                posteriores[classe] = priori + verossimilhanca
             
             # Retornar classe com maior probabilidade a posteriori
-            predictions.append(max(posteriors, key=posteriors.get))
+            predicoes.append(max(posteriores, key=posteriores.get))
         
-        return np.array(predictions)
+        return np.array(predicoes)
